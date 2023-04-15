c2807239f0ef55cc4196f5a6401803b877b86f11
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-04-15 19:11:49 +0200

    test the main.c

--- scr/main.c
+++ scr/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 int main() {
-    printf("%d,%5.2f", 1, 1.1);
+    float a=1.0;
+    float b=1.12;
+    float c;
+    float max(float a, float b);
+    printf("input the values of a and b: \n");
+    scanf("a=%f,b=%f", &a, &b);
+    c = max(a,b);
+    printf("a=%5.2f,b=%5.2f", a, b);
+    printf("\nthe max among a and b is: %5.2f", c);
     return 0;
+}
+
+float max(float a, float b) {
+    float c;
+    if (a >= b) {
+        c = a;
+    } else {
+        c = b;
+    }
+    return c;
 }
