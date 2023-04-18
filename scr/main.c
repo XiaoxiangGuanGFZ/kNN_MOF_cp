46552524ea603f35ea9b6b879599ab1603c5bf54
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-04-18 14:10:05 +0200

    update main.c

--- scr/main.c
+++ scr/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include "func_IO_data.c"
+
 int main() {
     float a=1.0;
     float b=1.12;
     float c;
-    float max(float a, float b);
+    // float max(float a, float b);
     printf("input the values of a and b: \n");
     scanf("a=%f,b=%f", &a, &b);
     c = max(a,b);
@@ -12,12 +14,3 @@ int main() {
     return 0;
 }
 
-float max(float a, float b) {
-    float c;
-    if (a >= b) {
-        c = a;
-    } else {
-        c = b;
-    }
-    return c;
-}
