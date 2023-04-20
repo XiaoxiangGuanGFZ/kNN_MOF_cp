9e1a9c1c0f9aa3c2752caa3d18cfaef785079577
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-04-20 22:13:53 +0200

    modify import_df_cp()

--- scr/func_IO_data.c
+++ scr/func_IO_data.c
@@ -5,35 +5,32 @@
 
 #define MAXCHAR 2000
 
-int main(){
-
-    char fname[50] = "D:\\kNN_MOF_cp\\data\\cp_series.txt";
-    int import_df_cp(char fname[100], int nrow, int ncol, int df[][5]);
-
-    int df_cp[100000][5];
-    int nrow=15553, ncol=5;
-    import_df_cp(fname, nrow, ncol, df_cp);
-    // printf("\nthe rows in data: %d\n", j);
-    printf("the first row:\n %d,%d,%d,%d,%d \n", df_cp[0][0], df_cp[0][1],df_cp[0][2],df_cp[0][3],df_cp[0][4]);
-    printf("the last row:\n %d", df_cp[nrow-1][ncol-1]);
-    return 0;
-}
+// int main(){
+//     char fname[50] = "D:\\kNN_MOF_cp\\data\\cp_series.txt";
+//     int import_df_cp(char fname[100], int nrow, int ncol, int df[][5]);
+//     int df_cp[100000][4];
+//     int nrow=15553, ncol=4;
+//     import_df_cp(fname, nrow, ncol, df_cp);
+//     // printf("\nthe rows in data: %d\n", j);
+//     printf("the first row:\n %d,%d,%d,%d,%d \n", df_cp[0][0], df_cp[0][1],df_cp[0][2],df_cp[0][3],df_cp[0][4]);
+//     printf("the last row:\n %d", df_cp[nrow-1][ncol-1]);
+//     return 0;
+// }
 
 int import_df_cp(
-    char fname[100],
-    int nrow, 
+    char fname[],
     int ncol,
-    int df[][5] 
+    int df[][4] 
 ) {
     /*********************
-    main function:
+    Main function:
         import the circulation pattern classification results as an array
-    parameters:
+    Parameters:
         fname: the file path, together with the file name of CP data
-        nrow: the number of rows in the data file
         ncol: the number of columns in the data file
-    output:
-        return an array (int type) with the size of nrow*ncol
+    Return:
+        bring an array (int type) with the size of nrow*ncol back to main() function;
+        the return value of the function: the number of rows in the data file
     *********************/
     FILE *fp;
     char row[MAXCHAR];
@@ -53,9 +50,9 @@ int import_df_cp(
         // the fgets() function comes from <stdbool.h>
         // Reads characters from stream and stores them as a C string
         fgets(row, MAXCHAR, fp); 
-        if (j==0) {
-            printf("Row: %s", row);
-        }
+        // if (j==0) {
+        //     printf("Row: %s", row);
+        // }
         
         token = strtok(row, ",");  
         /*
@@ -71,51 +68,5 @@ int import_df_cp(
         j=j+1;
     }
     fclose(fp);
-    return 0;
-}
-
-void import_global(
-    char fname[100]
-){
-    /**************
-     * import the global parameters into memory for disaggregation algorithm
-     * 
-     * -- Parameters:
-     *      fname: a string (1-D character array), file path and name of the global parameters
-     * -- Output:
-     *      return a structure containing the key fields
-     * ********************/
-    FILE *fp;
-    int j;
-    char row[MAXCHAR];  // a row (string) read from file
-    char *token;
-
-    if ((fp=fopen(fname, "r")) == NULL) {
-        printf("cannot open file\n");
-        exit(0);
-    }
-    j = 0;  // from the first row 
-    while (!feof(fp))
-    {
-        // the fgets() function comes from <stdbool.h>
-        // Reads characters from stream and stores them as a C string
-        fgets(row, MAXCHAR, fp); 
-        if (j==0) {
-            printf("Row: %s", row);
-        }
-        
-        token = strtok(row, ",");  
-        /*
-        strtok() fucntion: https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
-        */
-        for (i=0; i < ncol; i++) {
-            // the function atoi() comes from <stdlib.h>
-            // convert string into integer
-            df[j][i] = atoi(token);  
-            token = strtok(NULL, ",");  // the function strtok() comes from <string.h>
-        }
-        j=j+1;
-    }
-    fclose(fp);
-
+    return j-1;
 }
