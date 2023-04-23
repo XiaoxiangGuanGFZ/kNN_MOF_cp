a4eb1b2a43b7740e413b3b227afaf915dbb64b67
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-04-23 10:41:09 +0200

    create the hourly obs rr data import function

--- scr/main.c
+++ scr/main.c
@@ -15,6 +15,14 @@ struct df_rr_d
     struct Date date;    
     double *p_rr;
 };
+struct df_rr_h
+{
+    /* data */
+    struct Date date;    
+    double (*rr_h)[24];
+    double *rr_d;
+};
+
 struct Para_global
     {
         /* global parameters */
@@ -91,15 +99,25 @@ void main(int argc, char * argv[]) {
         df_rr_daily[nrow_rr_d-1].date.y,df_rr_daily[nrow_rr_d-1].date.m,df_rr_daily[nrow_rr_d-1].date.d
     );
     
-    // int i,j;
-    // for (i=0;i < 10;i++) {
-    //     printf("\ndate: %d-%d-%d: \n", df_rr_daily[i].date.y, df_rr_daily[i].date.m, df_rr_daily[i].date.d);
-    //     for (j=0; j < Para_df.N_STATION; j++) {
-    //         printf("%3.1f,", *((df_rr_daily[i]).p_rr+j));
-    //     }
-    // }
-
     /****** import hourly rainfall data (obs as fragments) *******/
+    int import_dfrr_h(
+        char FP_hourly[], 
+        int N_STATION,
+        struct df_rr_h *p_rr_h
+    );
+    int ndays_h;
+    struct df_rr_h df_rr_hourly[10000];
+    ndays_h = import_dfrr_h(Para_df.FP_HOURLY, Para_df.N_STATION, df_rr_hourly);
+    printf("------ Import hourly rr data (Done) ------ \n");
+    printf("* total hourly obs days: %d\n", ndays_h);
+    printf("* the first day: %d-%d-%d\n", df_rr_hourly[0].date.y, df_rr_hourly[0].date.m, df_rr_hourly[0].date.d);
+    printf(
+        "* the last day: %d-%d-%d\n", 
+        df_rr_hourly[ndays_h-1].date.y, df_rr_hourly[ndays_h-1].date.m, df_rr_hourly[ndays_h-1].date.d
+    );
+    /****** Disaggregation: kNN_MOF_cp *******/
+    
+
 
 }
 
@@ -194,9 +212,16 @@ int import_dfrr_d(
     int N_STATION,
     struct df_rr_d *p_rr_d
 ) {
-    /******************
-    
-    *******************/
+    /**************
+     * Main:
+     *  import daily rainfall data (tobe disaggregated) into memory
+     * Parameters: 
+     *  FP_daily: a string, storing the file path and name of daily rr data file
+     *  N_STATION: the number of rainfall stations in disaggrgeation 
+     *  p_rr_d: name of structure df_rr_d array
+     * Return:
+     *  output the number of days (rows)
+     * ****************/
     // char FP_daily[]="D:/kNN_MOF_cp/data/rr_obs_daily.csv";  // key parameter
     FILE *fp_d;
     if ((fp_d=fopen(FP_daily, "r")) == NULL) {
@@ -224,5 +249,95 @@ int import_dfrr_d(
         i=i+1;
     }
     nrow = i;
+    // int i,j;
+    // for (i=0;i < 10;i++) {
+    //     printf("\ndate: %d-%d-%d: \n", df_rr_daily[i].date.y, df_rr_daily[i].date.m, df_rr_daily[i].date.d);
+    //     for (j=0; j < Para_df.N_STATION; j++) {
+    //         printf("%3.1f,", *((df_rr_daily[i]).p_rr+j));
+    //     }
+    // }
     return nrow;
+}
+
+int import_dfrr_h(
+    char FP_hourly[], 
+    int N_STATION,
+    struct df_rr_h *p_rr_h
+) {
+    /**************
+     * Main:
+     *  import hourly rainfall observations into memory
+     * Parameters: 
+     *  FP_hourly: a string, storing the file path and name of hourly rr data file
+     *  N_STATION: the number of rainfall stations in disaggrgeation 
+     *  p_rr_h: name of structure df_rr_h array
+     * Return:
+     *  output the number of hourly observation days
+     * ****************/
+    // char FP_hourly[]="D:/kNN_MOF_cp/data/rr_obs_hourly.csv";
+    FILE *fp_h;
+    if ((fp_h=fopen(FP_hourly, "r")) == NULL) {
+        printf("Cannot open hourly rr data file!\n");
+        exit(0);
+    }
+    // struct df_rr_h df_rr_hourly[10000];
+    struct df_rr_h *p_df_rr_h;  // pointer of df_rr_h; for iteration
+    
+    char *token;
+    char row[MAXCHAR];
+    int i, j, h, nrow_total, ndays;
+    // int N_STATION = 134;
+    
+    i=0;
+    p_df_rr_h = p_rr_h; // initialize
+    while (!feof(fp_h)) {
+        fgets(row, MAXCHAR, fp_h);
+        if (i%24 == 0) {
+            /* %: remainder after division (modulo division)*/
+            (p_df_rr_h->date).y = atoi(strtok(row, ","));
+            (p_df_rr_h->date).m = atoi(strtok(NULL, ","));
+            (p_df_rr_h->date).d = atoi(strtok(NULL, ","));
+            p_df_rr_h->rr_h = calloc(N_STATION, sizeof(double) * 24);  // allocate memory (stack)
+        } else {
+            token = strtok(row, ","); token = strtok(NULL, ","); token = strtok(NULL, ",");
+        }
+        h = atoi(strtok(NULL, ","));
+        for (j=0; j < N_STATION; j++){
+            token = strtok(NULL, ",");
+            *(*(p_df_rr_h->rr_h + j) + h) = atof(token);
+        }
+        if (i%24 == 23) {
+            p_df_rr_h++;
+        }
+        i=i+1;
+    }
+    nrow_total = i;  // the total number of row in the data file
+    ndays = p_df_rr_h - p_rr_h;  // the exact size of struct p_rr_h array
+    /**** aggregate the hourly rr into daily scale ****/
+    for (p_df_rr_h = p_rr_h; p_df_rr_h<p_rr_h + ndays; p_df_rr_h++) {
+        p_df_rr_h->rr_d = (double *)malloc(N_STATION*sizeof(double)); // allocate memory (stack)
+        for (j=0;j<N_STATION;j++) {
+            *(p_df_rr_h->rr_d+j) = 0;
+            for (h=0;h<24;h++) {
+                *(p_df_rr_h->rr_d+j) += p_df_rr_h->rr_h[j][h];
+            }
+        }
+    }
+    /* check the results */ 
+    // printf("the total rows: %d\n", nrow_total);
+    // printf("total obs days: %d\n", ndays);
+    // p_df_rr_h = p_rr_h + 11;
+    // printf("date: %d-%d-%d\n", p_df_rr_h->date.y, p_df_rr_h->date.m, p_df_rr_h->date.d);
+    
+    // for (h=0; h < 24; h++) {
+    //     printf("H: %d\n", h);
+    //     for (j=0; j < N_STATION; j++){
+    //         printf("%3.1f\t", p_df_rr_h->rr_h[j][h]);
+    //     }
+    //     printf("\n");
+    // }
+    // for (j=0;j<N_STATION;j++) {
+    //     printf("%3.1f\t", p_df_rr_h->rr_d[j]);
+    // }
+    return ndays;
 }
--- scr/test.c
+++ scr/test.c
@@ -1,146 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "func_IO_data.c"
 
-# define MAXCHAR 2000
-struct Para_global
-    {
-        /* global parameters */
-        char FP_DAILY[100];
-        char FP_CP[100];
-        char FP_HOURLY[100];
-        int START_YEAR;
-        int START_MONTH;
-        int START_DAY;
-        int N_STATION;
-        char T_CP[10];
-        char SEASON[10];
-        int CONTINUITY;
-    };
+# define MAXCHAR 4000
+struct Date {
+    int y;
+    int m;
+    int d;
+};
 
-void main(int argc, char * argv[]) {
-    /*
-    int argc: the number of parameters of main() function;
-    char *argv[]: pointer array
-    */
-    /* char fname[100] = "D:/kNN_MOF_cp/data/global_para.txt";
-        this should be the only extern input for this program */
-    struct Para_global Para_df = {
-        "D:/kNN_MOD_cp/data/test_rr_daily.txt",
-        "D:/kNN_MOF_cp/data/test_cp_series.txt",
-        "D:/kNN_MOF_cp/data/test_rr_obs_hourly.txt",
-        1970,
-        5,
-        21,
-        15,
-        "TRUE",
-        "TRUE",
-        1
-    };  // define the parameter structure and initialization
-    struct Para_global * pp;
-    pp = &Para_df;
-    void import_global(char fname[], struct Para_global *pp);  // function declaration
-    /* import the global parameters 
-    parameter from main() function, pointer array
-    argv[0]: pointing to the first string from command line (the executable file)
-    argv[1]: pointing to the second string (parameter): file path and name of global parameter file.
-    */
-    import_global(*(++argv), pp);  
-    /******* import circulation pattern series *********/
-    int df_cp[100000][4];
-    int nrow_cp, ncol_cp = 4;  // the number of CP data columns: 4 (y, m, d, cp)
-    nrow_cp = import_df_cp(Para_df.FP_CP, ncol_cp, df_cp);
-    printf("------ Import CP data series (Done) ------ \n");
-    printf("* the first row: %d,%d,%d,%d \n", df_cp[0][0], df_cp[0][1], df_cp[0][2], df_cp[0][3]);
-    printf("* number of CP data rows: %d\n", nrow_cp);
-    /****** import hourly rainfall data (obs as fragments) *******/
+struct df_rr_h
+{
+    /* data */
+    struct Date date;    
+    double (*rr_h)[24];
+    double *rr_d;
+};
 
-}
-
-void import_global(
-    char fname[], struct Para_global *pp
-){
-    /**************
-     * import the global parameters into memory for disaggregation algorithm
-     * 
-     * -- Parameters:
-     *      fname: a string (1-D character array), file path and name of the global parameters
-     * -- Output:
-     *      return a structure containing the key fields
-     * ********************/
-    
-    char row[MAXCHAR];
-    FILE *fp;
-    char *token;
-    char *token2;
-    int i;
-    
-    if ((fp=fopen(fname, "r")) == NULL) {
-        printf("cannot open global parameter file\n");
+void main() {
+    char FP_hourly[]="D:/kNN_MOF_cp/data/rr_obs_hourly.csv";
+    FILE *fp_h;
+    if ((fp_h=fopen(FP_hourly, "r")) == NULL) {
+        printf("Cannot open hourly rr obs data file!\n");
         exit(0);
     }
-    while (!feof(fp))
-    {
-        // the fgets() function comes from <stdbool.h>
-        // Reads characters from stream and stores them as a C string
-        fgets(row, MAXCHAR, fp); 
-        if (row != NULL && strlen(row) > 1) {
-            /*non-empty row(string)*/
-            if (row[0] != '#') {
-                /* the first character of row should not be # */
-                for (i=0;i<strlen(row);i++) {
-                    /* remove all the characters after # */
-                    if (row[i] == '#') {
-                        row[i] = '\0';
-                    }
-                }
-                // printf("it is a valid row!\n");
-                // printf("Row: %s", row);
-                /*assign the values to the parameter structure: key-value pairs*/
-                token = strtok(row, ",");   // the first column: key
-                token2 = strtok(NULL, ",\n");  // the second column: value
-                // printf("token: %s\n", token);
-                if (strcmp(token, "FP_DAILY") == 0) {
-                    strcpy(pp->FP_DAILY, token2);
-                } else if (strcmp(token, "FP_CP") == 0) {
-                    strcpy(pp->FP_CP, token2);
-                } else if (strcmp(token, "FP_HOURLY") == 0) {
-                    strcpy(pp->FP_HOURLY, token2);
-                } else if (strcmp(token, "SEASON") == 0) {
-                    strcpy(pp->SEASON, token2);
-                } else if (strcmp(token, "T_CP") == 0) {
-                    strcpy(pp->T_CP, token2);
-                } else if (strcmp(token, "N_STATION") == 0) {
-                    pp->N_STATION = atoi(token2);
-                } else if (strcmp(token, "START_YEAR") == 0) {
-                    pp->START_YEAR = atoi(token2);
-                } else if (strcmp(token, "START_MONTH") == 0) {
-                    pp->START_MONTH = atoi(token2);
-                } else if (strcmp(token, "START_DAY") == 0) {
-                    pp->START_DAY = atoi(token2);
-                } else if (strcmp(token, "CONTINUITY") == 0) {
-                    pp->CONTINUITY = atoi(token2);   
-                } else {
-                    printf(
-                        "Error in opening global parameter file: unrecognized parameter field!"
-                    );
-                    exit(0);
-                }
-                
+    struct df_rr_h df_rr_hourly[10000];
+    struct df_rr_h *p_df_rr_h;
+    
+    char *token;
+    char row[MAXCHAR];
+    int i, j, h, nrow_total, ndays;
+    int N_STATION = 134;
+
+    i=0;
+    p_df_rr_h = df_rr_hourly;
+    while (!feof(fp_h)) {
+        fgets(row, MAXCHAR, fp_h);
+        if (i%24 == 0) {
+            /* %: remainder after division (modulo division)*/
+            (p_df_rr_h->date).y = atoi(strtok(row, ","));
+            (p_df_rr_h->date).m = atoi(strtok(NULL, ","));
+            (p_df_rr_h->date).d = atoi(strtok(NULL, ","));
+            p_df_rr_h->rr_h = calloc(N_STATION, sizeof(double) * 24);
+        } else {
+            token = strtok(row, ","); token = strtok(NULL, ","); token = strtok(NULL, ",");
+        }
+        h = atoi(strtok(NULL, ","));
+        for (j=0; j < N_STATION; j++){
+            token = strtok(NULL, ",");
+            *(*(p_df_rr_h->rr_h + j) + h) = atof(token);
+        }
+        if (i%24 == 23) {
+            p_df_rr_h++;
+        }
+        i=i+1;
+    }
+    nrow_total = i;  // the total number of row in the data file
+    ndays = p_df_rr_h - df_rr_hourly;  // the exact size of struct df_rr_hourly array
+    /* aggregate the hourly rr into daily scale */
+    for (p_df_rr_h = df_rr_hourly;p_df_rr_h<df_rr_hourly + ndays; p_df_rr_h++) {
+        p_df_rr_h->rr_d = (double *)malloc(N_STATION*sizeof(double));
+        for (j=0;j<N_STATION;j++) {
+            *(p_df_rr_h->rr_d+j) = 0;
+            for (h=0;h<24;h++) {
+                *(p_df_rr_h->rr_d+j) += p_df_rr_h->rr_h[j][h];
             }
         }
     }
-    fclose(fp);
-    printf("------ Global parameter import completed: -----\n");
-    printf(
-        "FP_DAILY: %s\nFP_HOULY: %s\nFP_CP: %s\nSTART_YEAR: %d\tSTART_MONTH: %d\tSTART_DAY: %d\n",
-        pp->FP_DAILY, pp->FP_HOURLY, pp->FP_CP, pp->START_YEAR, pp->START_MONTH, pp->START_DAY
-    );
-    printf("------ Disaggregation parameters: -----\n");
-    printf(
-        "T_CP: %s\nSEASON: %s\nN_STATION: %d\nCONTINUITY: %d\n",
-        pp->T_CP, pp->SEASON, pp->N_STATION, pp->CONTINUITY
-    );
-}
+    // check the results
+    printf("the total rows: %d\n", nrow_total);
+    printf("total obs days: %d\n", ndays);
+    p_df_rr_h = df_rr_hourly + 11;
+    printf("date: %d-%d-%d\n", p_df_rr_h->date.y, p_df_rr_h->date.m, p_df_rr_h->date.d);
+    
+    for (h=0; h < 24; h++) {
+        printf("H: %d\n", h);
+        for (j=0; j < N_STATION; j++){
+            printf("%3.1f\t", p_df_rr_h->rr_h[j][h]);
+        }
+        printf("\n");
+    }
+    for (j=0;j<N_STATION;j++) {
+        printf("%3.1f\t", p_df_rr_h->rr_d[j]);
+    }
 
+}
