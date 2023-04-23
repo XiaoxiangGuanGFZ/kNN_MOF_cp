dbbab5a7e3f7da9aab3e138ea566ec83d85977e2
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-04-23 23:29:45 +0200

    create continuity filtering function

--- scr/main.c
+++ scr/main.c
@@ -66,24 +66,23 @@ void main(int argc, char * argv[]) {
         1,
         0
     };  // define the parameter structure and initialization
-    struct Para_global * pp;
-    pp = &Para_df;
-    void import_global(char fname[], struct Para_global *pp);  // function declaration
+    struct Para_global * p_gp;
+    p_gp = &Para_df;
+    void import_global(char fname[], struct Para_global *p_gp);  // function declaration
     /* import the global parameters 
     parameter from main() function, pointer array
     argv[0]: pointing to the first string from command line (the executable file)
     argv[1]: pointing to the second string (parameter): file path and name of global parameter file.
     */
-    import_global(*(++argv), pp);  
+    import_global(*(++argv), p_gp);  
     /******* import circulation pattern series *********/
     int import_df_cp(
         char fname[],
         struct df_cp *p_df_cp
     );
-    if (strcmp(pp->T_CP, "TRUE") == 0) {
-        // int df_cps[100000][4];
-        struct df_cp df_cps[MAXrow];
-        int nrow_cp;  // the number of CP data columns: 4 (y, m, d, cp)
+    struct df_cp df_cps[MAXrow];
+    int nrow_cp=0;  // the number of CP data columns: 4 (y, m, d, cp)
+    if (strcmp(p_gp->T_CP, "TRUE") == 0) {
         nrow_cp = import_df_cp(Para_df.FP_CP, df_cps);
         printf("------ Import CP data series (Done) ------ \n");
         printf("* number of CP data rows: %d\n", nrow_cp);
@@ -91,9 +90,7 @@ void main(int argc, char * argv[]) {
         printf("* the last day: %d-%d-%d \n", 
             df_cps[nrow_cp-1].date.y, df_cps[nrow_cp-1].date.m, df_cps[nrow_cp-1].date.d
         );
-        
     } else {
-        int df_cp=0;
         printf("------ Disaggregation conditioned only on seasonality (12 months) ------ \n");
     }
     /****** import daily rainfall data (to be disaggregated) *******/
@@ -134,13 +131,27 @@ void main(int argc, char * argv[]) {
         df_rr_hourly[ndays_h-1].date.y, df_rr_hourly[ndays_h-1].date.m, df_rr_hourly[ndays_h-1].date.d
     );
     /****** Disaggregation: kNN_MOF_cp *******/
-
-
+    void kNN_MOF(
+        struct df_rr_h *p_rrh,
+        struct df_rr_d *p_rrd,
+        struct df_cp *p_cp,
+        struct Para_global *p_gp,
+        int nrow_rr_d,
+        int ndays_h
+    );
+    kNN_MOF(
+        df_rr_hourly,
+        df_rr_daily,
+        df_cps,
+        p_gp,  // the pointer pointing to Para_df structure;
+        nrow_rr_d,
+        ndays_h
+    );
 
 }
 
 void import_global(
-    char fname[], struct Para_global *pp
+    char fname[], struct Para_global *p_gp
 ){
     /**************
      * import the global parameters into memory for disaggregation algorithm
@@ -183,25 +194,25 @@ void import_global(
                 token2 = strtok(NULL, ",\n");  // the second column: value
                 // printf("token: %s\n", token);
                 if (strcmp(token, "FP_DAILY") == 0) {
-                    strcpy(pp->FP_DAILY, token2);
+                    strcpy(p_gp->FP_DAILY, token2);
                 } else if (strcmp(token, "FP_CP") == 0) {
-                    strcpy(pp->FP_CP, token2);
+                    strcpy(p_gp->FP_CP, token2);
                 } else if (strcmp(token, "FP_HOURLY") == 0) {
-                    strcpy(pp->FP_HOURLY, token2);
+                    strcpy(p_gp->FP_HOURLY, token2);
                 } else if (strcmp(token, "SEASON") == 0) {
-                    strcpy(pp->SEASON, token2);
+                    strcpy(p_gp->SEASON, token2);
                 } else if (strcmp(token, "T_CP") == 0) {
-                    strcpy(pp->T_CP, token2);
+                    strcpy(p_gp->T_CP, token2);
                 } else if (strcmp(token, "N_STATION") == 0) {
-                    pp->N_STATION = atoi(token2);
+                    p_gp->N_STATION = atoi(token2);
                 } else if (strcmp(token, "WD") == 0) {
-                    pp->WD = atoi(token2);
+                    p_gp->WD = atoi(token2);
                 } else if (strcmp(token, "FP_OUT") == 0) {
-                    strcpy(pp->FP_OUT, token2);
+                    strcpy(p_gp->FP_OUT, token2);
                 } else if (strcmp(token, "FP_LOG") == 0) {
-                    strcpy(pp->FP_LOG, token2);
+                    strcpy(p_gp->FP_LOG, token2);
                 } else if (strcmp(token, "CONTINUITY") == 0) {
-                    pp->CONTINUITY = atoi(token2);   
+                    p_gp->CONTINUITY = atoi(token2);   
                 } else {
                     printf(
                         "Error in opening global parameter file: unrecognized parameter field!"
@@ -215,12 +226,12 @@ void import_global(
     printf("------ Global parameter import completed: -----\n");
     printf(
         "FP_DAILY: %s\nFP_HOULY: %s\nFP_CP: %s\nFP_OUT: %s\nFP_LOG: %s\n",
-        pp->FP_DAILY, pp->FP_HOURLY, pp->FP_CP, pp->FP_OUT, pp->FP_LOG
+        p_gp->FP_DAILY, p_gp->FP_HOURLY, p_gp->FP_CP, p_gp->FP_OUT, p_gp->FP_LOG
     );
     printf("------ Disaggregation parameters: -----\n");
     printf(
         "T_CP: %s\nSEASON: %s\nN_STATION: %d\nCONTINUITY: %d\nWD: %d\n",
-        pp->T_CP, pp->SEASON, pp->N_STATION, pp->CONTINUITY, pp->WD
+        p_gp->T_CP, p_gp->SEASON, p_gp->N_STATION, p_gp->CONTINUITY, p_gp->WD
     );
 }
 
@@ -267,13 +278,14 @@ int import_dfrr_d(
     }
     nrow = i;
     fclose(fp_d);
-    // int i,j;
-    // for (i=0;i < 10;i++) {
-    //     printf("\ndate: %d-%d-%d: \n", df_rr_daily[i].date.y, df_rr_daily[i].date.m, df_rr_daily[i].date.d);
-    //     for (j=0; j < Para_df.N_STATION; j++) {
-    //         printf("%3.1f,", *((df_rr_daily[i]).p_rr+j));
-    //     }
-    // }
+    
+    for (i=16;i < 17;i++) {
+        printf("\ndate: %d-%d-%d: \n", (p_rr_d + i)->date.y, (p_rr_d + i)->date.m, (p_rr_d + i)->date.d);
+        for (j=0; j < N_STATION; j++) {
+            printf("%3.1f\t", *((p_rr_d + i)->p_rr+j));
+        }
+    }
+    printf("\n");
     return nrow;
 }
 
@@ -343,21 +355,21 @@ int import_dfrr_h(
         }
     }
     /* check the results */ 
-    // printf("the total rows: %d\n", nrow_total);
-    // printf("total obs days: %d\n", ndays);
-    // p_df_rr_h = p_rr_h + 11;
-    // printf("date: %d-%d-%d\n", p_df_rr_h->date.y, p_df_rr_h->date.m, p_df_rr_h->date.d);
-    
-    // for (h=0; h < 24; h++) {
-    //     printf("H: %d\n", h);
-    //     for (j=0; j < N_STATION; j++){
-    //         printf("%3.1f\t", p_df_rr_h->rr_h[j][h]);
-    //     }
-    //     printf("\n");
-    // }
-    // for (j=0;j<N_STATION;j++) {
-    //     printf("%3.1f\t", p_df_rr_h->rr_d[j]);
-    // }
+    p_df_rr_h = p_rr_h + 16;
+    printf("%d-%d-%d: \n", p_df_rr_h->date.y, p_df_rr_h->date.m, p_df_rr_h->date.d);
+    for (h=0; h < 24; h++) {
+        printf("H: %d\n", h);
+        for (j=0; j < N_STATION; j++){
+            printf("%3.1f\t", p_df_rr_h->rr_h[j][h]);
+        }
+        printf("\n");
+    }
+    printf("daily: \n");
+    for (j=0; j < N_STATION; j++){
+            printf("%3.1f\t", p_df_rr_h->rr_d[j]);
+    }
+    printf("\n");
+
     return ndays;
 }
 
@@ -404,3 +416,142 @@ int import_df_cp(
     return j;  // the number of rows
 }
 
+void kNN_MOF(
+    struct df_rr_h *p_rrh,
+    struct df_rr_d *p_rrd,
+    struct df_cp *p_cp,
+    struct Para_global *p_gp,
+    int nrow_rr_d,
+    int ndays_h
+) {
+    /*******************
+     * Description:
+     *  algorithm: k-nearest neighbouring sampling, method-of-fragments, circulation patterns and (or) seasonality
+     *  scale: daily2hourly, multiple rain gauges simultaneously
+     * Parameters:
+     *  nrow_rr_d: the number of rows in daily rr data file
+     *  ndays_h: the number of observations of hourly rr data
+     *  
+     * *****************/
+    int i, j, h, k, Toggle_wd;
+    int skip=0;
+    int n_candidates = 0; // the number of the candidates 
+    struct df_rr_h df_rr_h_out;
+    // struct df_rr_h df_rr_h_candidates[100];
+    // p_gp->CONTINUITY: 1, skip = 0;
+    // p_gp->CONTINUITY: 3, skip = 1;
+    skip = (p_gp->CONTINUITY - 1) / 2;
+    int pool_cans[MAXrow];  // the index of the candidates
+    int n_cans=0;  // the number of candidates
+    int Toggle_CONTUINITY(
+        struct df_rr_h *p_rrh,
+        struct df_rr_d *p_rrd,
+        struct Para_global *p_gp,
+        int ndays_h,
+        int pool_can[],
+        int WD
+    );
+    for (i=skip; i < nrow_rr_d-skip; i++) { //i=skip
+        // iterate each (possible) target day
+        df_rr_h_out.date = (p_rrd + i)->date;
+        df_rr_h_out.rr_h = calloc(p_gp->N_STATION, sizeof(double) * 24);  // allocate memory (stack);
+        Toggle_wd = 0;  // initialize with 0 (non-rainy)
+        for (j=0; j < p_gp->N_STATION; j++) {
+            if (*((p_rrd + i)->p_rr + j) > 0.0) {
+                // any gauge with rr > 0.0
+                Toggle_wd = 1;
+            }
+        }
+        printf("Targetday: %d-%d-%d: %d\n", 
+            (p_rrd + i)->date.y, (p_rrd + i)->date.m, (p_rrd + i)->date.d, Toggle_wd
+        );
+        if (Toggle_wd == 0) {
+            // this is a non-rainy day; all 0.0
+            n_cans = -1;
+            for (j=0; j<p_gp->N_STATION; j++){
+                for (h=0; h<24; h++) {
+                    df_rr_h_out.rr_h[j][h] = 0.0;
+                }
+            }
+        } else {
+            // this is a rainy day; we will disaggregate it.
+            n_cans = Toggle_CONTUINITY(
+                p_rrh,
+                p_rrd + i,
+                p_gp,
+                ndays_h,
+                pool_cans,
+                0  // WD == 0, strict
+            );
+            if (n_cans == 0) {
+                n_cans = Toggle_CONTUINITY(
+                            p_rrh,
+                            p_rrd + i,
+                            p_gp,
+                            ndays_h,
+                            pool_cans,
+                            1  // WD == 1, flexible
+                        );
+            }
+            
+        }
+        printf("%d-%d-%d: %d\n", (p_rrd+i)->date.y, (p_rrd+i)->date.m, (p_rrd+i)->date.d, n_cans);
+    }
+}
+
+int Toggle_CONTUINITY(
+    struct df_rr_h *p_rrh,
+    struct df_rr_d *p_rrd,
+    struct Para_global *p_gp,
+    int ndays_h,
+    int pool_can[],
+    int WD // Wet-dry status matching: strict:0; flexible:1
+){
+    /*****************
+     * 
+     * ***************/
+    int i, j, k, skip, toggle_WD;
+    // toggle_WD: whether wet-dry status match
+    int n_cans=0;  // number of candidates fullfilling the CONTINUITY criteria (output)
+    skip = (p_gp->CONTINUITY - 1) / 2; // p_gp->CONTINUITY == 1: skip=0; p_gp->CONTINUITY == 3: skip=1
+    
+    for (k=skip; k < ndays_h-skip; k++) { 
+        // iterate each day in hourly observations
+        toggle_WD = 1;
+        for (i=0-skip; i < 1+skip; i++) {
+            // here we have to compare vectors between candidates and target; 
+            //      CONTINUITY=1: skip=0; CONTINUITY=3: skip=1; 
+            for (j=0; j < p_gp->N_STATION; j++){
+                if (WD == 0) {
+                    /*strict wet-dry matching*/
+                    // the wet-dry status in each vector should be completely the same with each other.
+                    if (
+                        !(
+                            ((p_rrd + i)->p_rr[j] > 0.0) == ((p_rrh + k + i)->rr_d[j] > 0.0)
+                        )
+                    ) {
+                        toggle_WD = 0;
+                    } else {
+                        continue;
+                    }
+                } else {
+                    // WD == 1
+                    /*flexible wet-dry matching*/
+                    if (
+                        (int)(
+                            ((p_rrd + i)->p_rr[j] > 0.0) == ((p_rrh + k + i)->rr_d[j] >= 0.0)
+                        ) < 1
+                    ) {
+                        toggle_WD = 0;
+                    }
+                }
+            }
+        }
+        if (toggle_WD == 1) {
+            *(pool_can + n_cans) = k;
+            n_cans++;
+            // printf("n_cans: %d\n", n_cans);
+        }
+    }
+    return n_cans;
+}
