33811224d930cc9140ec2a21f117a1b9551255c4
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2023-10-29 18:44:18 +0100

    update validate.r

--- scr/validate.R
+++ scr/validate.R
@@ -42,3 +42,84 @@ for (i in 1:N_STATION){
 }
   
 
+# ----- debug the candidates sampling -----
+library(tidyverse)
+ws <- "D:/kNN_MOF_cp/"
+n = 300
+
+df_rwg <- read.table(
+	paste0(ws, "data/rr_sim_daily_long.csv"),
+	header = F, sep = ","
+) %>% `colnames<-`(c('y', 'm', 'd', paste0("s", 1:n)))
+
+df_obs_h <- read.table(
+	paste0(ws, "data/rr_obs_hourly_long.csv"),
+	header = F, sep = ','
+) %>% `colnames<-`(c('y', 'm', 'd', 'h', paste0("s", 1:n)))
+
+for (i in 1:n) {
+	df_aggre = df_obs_h[, c(1:4, i + 4)] %>% 
+	`colnames<-`(c('y', 'm', 'd', 'h', 'rr')) %>% 
+	group_by(y,m,d) %>%
+	summarise(rr = sum(rr))
+	if (i == 1) {
+		df_obs_d = df_aggre
+	} else {
+		df_obs_d = cbind(df_obs_d, df_aggre[, 4])
+	}
+}
+df_obs_d <- df_obs_d %>% `colnames<-`(c('y', 'm', 'd', paste0("s", 1:n)))
+write.table(
+	df_obs_d,
+	paste0(ws, "data/rr_obs_daily_long.csv"),
+	col.names = F, row.names = F, append = F, quote = F, sep = ","
+)
+
+df_cp <- read.table(
+	paste0(ws, "data/cp_series_long.csv"),
+	header = F, sep = ','
+) %>% `colnames<-`(c('y', 'm', 'd', 'cp'))
+
+df_index <- read.table(
+	paste0(ws, "data/candidates.txt"),
+	header = T
+)
+
+# --- check the CP class
+cp_obs = 2
+
+for (i in 1:dim(df_index)[1]) {
+	df_can <- df_obs_d[df_index$index[i]+1, ] %>% as.data.frame()
+	cp_can = df_cp[df_cp[,1] == df_can[1,1] & df_cp[,2] == df_can[1,2] & df_cp[,3] == df_can[1,3], 4]
+	print(cp_can)
+}
+
+# --- check the wet-dry status
+
+for (i in 1:dim(df_index)[1]) {
+	df_can <- df_obs_d[df_index$index[i]+1, ] %>% as.data.frame()
+	for (j in 1:n) {
+		if (df_rwg[1, 3 + j] > 0.0 & df_can[1, 3 + j] <= 0.0) {
+			print("mismatch")
+		}
+	}
+}
+
+df_cans <- df_obs_d[df_index$index+1, ] %>% as.data.frame()
+write.table(
+	df_cans,
+	paste0(ws, "data/rr_obs_daily_long_candadites.csv"),
+	col.names = F, row.names = F, append = F, quote = F, sep = ","
+)
+
+df_random <- read.table(
+	paste0(ws, "data/random_output.txt"),
+	header = F, sep = ','
+) %>% `colnames<-`(c("rd", "fragment"))
+
+for (i in 1:dim(df_random)[1]) {
+	if (!(df_random$fragment[i] %in% df_index[,1])) {
+		print0(df_random$fragment[i])
+		
+	}
+}
